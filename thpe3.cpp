b54c47bfbc5c59369be1f2be33f8010357305011
chase-but-not-real-name <101519145+chase-but-not-real-name@users.noreply.github.com>
2022-03-23 07:57:41 -0600

    Update thpe3.cpp

--- thpe3.cpp
+++ thpe3.cpp
@@ -1,2 +1,22 @@
 #include "thpe3.h"
 
+int getRandom(int seed){
+    static default_random_engine generator( seed );
+    uniform_int_distribution<int> distribution(0,51);
+    return distribution( generator );
+}
+
+
+void DrawHand(int Hand[], int seed){
+    for(int i = 0; i < 5; i++){
+        Hand[i] = getRandom(seed);//draws random numbers between 0 and 51
+        
+        for(int j = 0; j < i; j++){
+            if(Hand[j] == Hand[i]){
+                //if dupelicate then draw another card
+                Hand[i] = getRandom(seed);
+                j = -1;
+            }
+        }
+    }
+}
