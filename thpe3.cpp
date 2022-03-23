82b1598e6871f89671d0bc0a74482602c67e5a6f
chase-but-not-real-name <101519145+chase-but-not-real-name@users.noreply.github.com>
2022-03-23 08:01:01 -0600

    Update thpe3.cpp

--- thpe3.cpp
+++ thpe3.cpp
@@ -6,16 +6,32 @@ int getRandom(int seed){
     return distribution( generator );
 }
 
-
 void DrawHand(int Hand[], int seed){
     for(int i = 0; i < 5; i++){
-        Hand[i] = getRandom(seed);//draws random numbers between 0 and 51
-        
+        Hand[i] = getRandom(seed);
+        //draws random numbers between 0 and 51
         for(int j = 0; j < i; j++){
             if(Hand[j] == Hand[i]){
                 //if dupelicate then draw another card
                 Hand[i] = getRandom(seed);
                 j = -1;
+            }
+        }
+    }
+}
+
+void sort(int ArrayN[], const int size){
+    //GOOD OLD BUBBLE SORT WOOOOOO MY FAVORITE (;
+    bool swapMade = true;
+    int i, j;
+    for(i = 0; i < size - 1 && swapMade; i++){
+        swapMade = false;
+        for(j = 0; j < size - 1 - i; j ++){
+            if( ArrayN[j] > ArrayN[j + 1] ){
+                int temp = ArrayN[j];
+                ArrayN[j] = ArrayN[j + 1];
+                ArrayN[j + 1] = temp;
+                swapMade = true;
             }
         }
     }
